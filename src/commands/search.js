eec4b22dc9de5abd79a0b753fd54c1cc6d1ec858
Pranjal Walia <pranjalwalia77@gmail.com>
2021-08-05 14:28:14 +0530

    feat: integrate server resonses
    - pending output formatting

--- src/commands/search.js
+++ src/commands/search.js
@@ -1,16 +1,38 @@
 const chalk = require("chalk");
+const axios = require("axios");
 const { prompt } = require("inquirer");
+const { baseURl } = require("../config");
 
 const questions = [
   {
     type: "input",
-    name: "firstname",
-    message: "Enter you query enclosed in quotes",
+    name: "query",
+    message: "Enter you query",
   },
 ];
 
-const search = () => {
-  prompt(questions);
+const search = async () => {
+  const input = await prompt(questions);
+  const res = await axios.get(`${baseURl}?query=${input.query}`);
+
+  //todo: format this response so it looks good
+  console.log(chalk.green(JSON.stringify(res.data)));
 };
 
-module.exports = { search };
+const stackoverflow = async () => {
+  const input = await prompt(questions);
+  const res = await axios.get(`${baseURl}/stackoverflow?query=${input.query}`);
+
+  //todo: format this response so it looks good
+  console.log(chalk.green(JSON.stringify(res.data)));
+};
+
+const youtube = async () => {
+  const input = await prompt(questions);
+  const res = await axios.get(`${baseURl}/youtube?query=${input.query}`);
+
+  //todo: format this response so it looks good
+  console.log(chalk.green(JSON.stringify(res.data)));
+};
+
+module.exports = { search, stackoverflow, youtube };
